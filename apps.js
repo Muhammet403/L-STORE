5ddc41ffc9074fb63021678d8350da775dd29ebe
Muhammet403 <filizcayli73@gmail.com>
2025-07-09 23:39:36 +0300

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,13 @@
+// data/apps.js
+
+const apps = [
+  {
+    id: 1,
+    name: "MojoLauncher",
+    description: "PojavLauncher'ı temel alan mobil bir LWJGL oyun başlatıcısı",
+    version: "1.0.0",
+    apkUrl: "https://example.com/mojo-launcher.apk"
+  }
+];
+
+module.exports = apps;
